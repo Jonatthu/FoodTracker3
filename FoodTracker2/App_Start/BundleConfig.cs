42373c9b8116b07ec7a6ade41fe810b2530d8f88
jonatthu <jonatthu@outlook.com>
2016-02-23 20:41:06 -0800

    asdasdsad

---
+++ FoodTracker2/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace FoodTracker2
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            "~/Scripts/jquery-1.9.1.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            "~/Scripts/bootstrap.js"));
+            bundles.Add(new StyleBundle("~/Css").Include(
+            "~/Content/bootstrap.css",
+            "~/Content/Site.css"));
+        }
+    }
+}
