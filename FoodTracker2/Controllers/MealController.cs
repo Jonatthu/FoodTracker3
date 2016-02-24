fdb09d195ad43b6aa5baa53e3d5a387177c9a777
pjf2180 <pjf2180@me.com>
2016-02-23 21:35:07 -0800

    Meal view added

---
+++ FoodTracker2/Controllers/MealController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodTracker2.Controllers
+{
+    public class MealController : Controller
+    {
+        // GET: Meal
+        public ActionResult Index(string id)
+        {
+            return View();
+        }
+    }
+}
