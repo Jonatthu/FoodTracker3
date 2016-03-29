0ff1d37fb583a3947de31e291edaf79cafbd41cc
pjf2180 <pjf2180@me.com>
2016-03-29 10:34:08 -0700

    database active

---
+++ FoodTracker3.Data/Migrations/201603220641203_PaulInitial.cs
@@ -0,0 +1,32 @@
+namespace FoodTracker3.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PaulInitial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Meals",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        MealName = c.String(nullable: false),
+                        AuthorName = c.String(),
+                        Description = c.String(nullable: false),
+                        ImageUrl = c.String(),
+                        Active = c.Boolean(nullable: false),
+                        Rating = c.Int(nullable: false),
+                        Date = c.DateTime(nullable: false, precision: 7, storeType: "datetime2"),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Meals");
+        }
+    }
+}
